1f32f63f5b1a70e1661c020cbf393d03a5b3c9d4
Bryan Dixon <javawolfpack@gmail.com>
2015-09-07 13:08:27 -0700

    Simple Input Example

---
+++ SimpleInput/simpleinput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+  string line;
+  getline(cin, line);
+  cout << line << endl;
+  cin >> line;
+  cout << line << endl;
+}
