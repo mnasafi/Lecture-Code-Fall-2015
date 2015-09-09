ff7e04c3101cda4100b7402fab048503a572b1f5
Bryan Dixon <javawolfpack@gmail.com>
2015-09-09 15:49:02 -0700

    Pass by method examples added

--- PointerExample/pointer.cpp
+++ PointerExample/pointer.cpp
@@ -3,6 +3,21 @@
 
 using namespace std;
 
+void passbyvalue(int value){
+  value = 4;
+  return;
+}
+
+void passbypointer(int *value){
+  *value = 5;
+  return;
+}
+
+void passbyreference(int &value){
+  value = 4;
+  return;
+}
+
 int main(int argc, char* argv[]){
   int size = -1;
   int *x;
@@ -14,6 +29,11 @@ int main(int argc, char* argv[]){
       x[i]=i;
     }
     for(int i=0;i<size;i++){
+      passbyvalue(x[i]);
+      cout << x[i] << ", ";
+      passbyreference(x[i]);
+      cout << x[i] << ", ";
+      passbypointer(&x[i]);
       cout << x[i] << endl;
     }
     delete x;
