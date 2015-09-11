7fa7a731959d5e57272f350f7e3c53b28c297209
Bryan Dixon <javawolfpack@gmail.com>
2015-09-11 15:46:58 -0700

    List.h Initially Added

---
+++ SimpleLinkedList/list.h
@@ -0,0 +1,22 @@
+#ifndef LIST_H
+#define LIST_H
+
+class List{
+  public:
+    List();
+    ~List();
+    void insert(int value);
+    void print();
+  private:
+    class Node{
+      int m_value;
+      Node *m_next;
+      Node(int value, Node *next){
+        m_value=value;
+        m_next=next;
+      }
+    };
+    Node *m_head;
+}
+
+#endif
